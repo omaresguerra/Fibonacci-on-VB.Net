02f4e1e7c8ee5fc9c30dd69000d5c7455f954a57
John Omar D. Esguerra <esguerrajohnomar@gmail.com>
2018-05-04 09:15:26 -0800

    First commit

---
+++ Fibonacci/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOk = New System.Windows.Forms.Button
+        Me.txtStart = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtSecond = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtEnd = New System.Windows.Forms.TextBox
+        Me.lstOutput = New System.Windows.Forms.ListBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.lblDisplay = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.lblPos = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.btnPosition = New System.Windows.Forms.Button
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.txtPosition = New System.Windows.Forms.TextBox
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnOk
+        '
+        Me.btnOk.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOk.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOk.Location = New System.Drawing.Point(171, 148)
+        Me.btnOk.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(125, 36)
+        Me.btnOk.TabIndex = 0
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = False
+        '
+        'txtStart
+        '
+        Me.txtStart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtStart.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStart.Location = New System.Drawing.Point(171, 14)
+        Me.txtStart.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtStart.Multiline = True
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(125, 33)
+        Me.txtStart.TabIndex = 1
+        Me.txtStart.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 22)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Starting number:"
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClear.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(171, 191)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(125, 36)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(19, 62)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(140, 18)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Second number:"
+        '
+        'txtSecond
+        '
+        Me.txtSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtSecond.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSecond.Location = New System.Drawing.Point(171, 55)
+        Me.txtSecond.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtSecond.Multiline = True
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(125, 33)
+        Me.txtSecond.TabIndex = 5
+        Me.txtSecond.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(24, 103)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(137, 18)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Ending number:"
+        '
+        'txtEnd
+        '
+        Me.txtEnd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtEnd.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEnd.Location = New System.Drawing.Point(171, 96)
+        Me.txtEnd.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtEnd.Multiline = True
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(125, 33)
+        Me.txtEnd.TabIndex = 7
+        Me.txtEnd.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.HorizontalScrollbar = True
+        Me.lstOutput.ItemHeight = 18
+        Me.lstOutput.Location = New System.Drawing.Point(316, 14)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(174, 364)
+        Me.lstOutput.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblDisplay)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.lblPos)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.btnPosition)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtPosition)
+        Me.GroupBox1.Location = New System.Drawing.Point(17, 241)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(279, 140)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Font = New System.Drawing.Font("Verdana", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDisplay.Location = New System.Drawing.Point(23, 106)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(0, 18)
+        Me.lblDisplay.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(7, 108)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(18, 16)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "is"
+        '
+        'lblPos
+        '
+        Me.lblPos.AutoSize = True
+        Me.lblPos.Font = New System.Drawing.Font("Verdana", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPos.Location = New System.Drawing.Point(152, 85)
+        Me.lblPos.Name = "lblPos"
+        Me.lblPos.Size = New System.Drawing.Size(0, 18)
+        Me.lblPos.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(6, 87)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(148, 16)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "The value at position"
+        '
+        'btnPosition
+        '
+        Me.btnPosition.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnPosition.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPosition.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPosition.Location = New System.Drawing.Point(175, 41)
+        Me.btnPosition.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.btnPosition.Name = "btnPosition"
+        Me.btnPosition.Size = New System.Drawing.Size(96, 33)
+        Me.btnPosition.TabIndex = 10
+        Me.btnPosition.Text = "Ok"
+        Me.btnPosition.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 23)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(152, 14)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Enter position number:"
+        '
+        'txtPosition
+        '
+        Me.txtPosition.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPosition.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPosition.Location = New System.Drawing.Point(12, 41)
+        Me.txtPosition.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtPosition.Multiline = True
+        Me.txtPosition.Name = "txtPosition"
+        Me.txtPosition.Size = New System.Drawing.Size(153, 33)
+        Me.txtPosition.TabIndex = 8
+        Me.txtPosition.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 393)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.btnOk)
+        Me.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fibonacci"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents txtStart As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSecond As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtEnd As System.Windows.Forms.TextBox
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtPosition As System.Windows.Forms.TextBox
+    Friend WithEvents btnPosition As System.Windows.Forms.Button
+    Friend WithEvents lblDisplay As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblPos As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Fibonacci/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+
+    Dim fibo As Double, _
+    first As Double, _
+    second As Double, _
+    ending As Double, _
+    position As Double
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+
+        fibo = 0
+        first = Val(txtStart.Text)
+        second = Val(txtSecond.Text)
+        ending = Val(txtEnd.Text)
+
+
+        If txtStart.Text = "" Or txtSecond.Text = "" Or txtEnd.Text = "" Then
+            MessageBox.Show("Enter number to proceed.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        ElseIf ending <= 1 And ending > 0 Then
+            MessageBox.Show("Enter higher number to proceed.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtEnd.Text = ""
+            txtEnd.Focus()
+
+        ElseIf ending <= 0 Then
+            MessageBox.Show("Enter positive number to proceed.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtEnd.Text = ""
+            txtEnd.Focus()
+        Else
+            btnOk.Enabled = False
+            lstOutput.Items.Add(first)
+            lstOutput.Items.Add(second)
+
+            For i As Integer = 1 To ending - 2
+                fibo = first + second
+                first = second
+                second = fibo
+                lstOutput.Items.Add(fibo.ToString)
+            Next
+
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtStart.Text = ""
+        txtSecond.Text = ""
+        txtEnd.Text = ""
+        txtPosition.Text = ""
+        lblPos.Text = ""
+        lblDisplay.Text = ""
+        txtStart.Focus()
+        lstOutput.Items.Clear()
+        btnOk.Enabled = True
+
+    End Sub
+
+    Private Sub btnPosition_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPosition.Click
+        position = Val(txtPosition.Text)
+        ending = Val(txtEnd.Text)
+
+        If txtStart.Text = "" Or txtSecond.Text = "" Or txtEnd.Text = "" Or txtPosition.Text = "" Then
+            MessageBox.Show("Enter number to proceed.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtStart.Focus()
+
+        ElseIf position <= 0 Then
+            MessageBox.Show("Out of index range!.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            lblDisplay.Text = ""
+            lblPos.Text = ""
+            txtPosition.Text = ""
+            txtPosition.Focus()
+        Else
+
+            If position <= ending Then
+                lblPos.Text = position
+                lblDisplay.Text = lstOutput.Items(position - 1)
+
+            Else
+                MessageBox.Show("Out of index range!.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                lblDisplay.Text = ""
+                lblPos.Text = ""
+                txtPosition.Text = ""
+                txtPosition.Focus()
+            End If
+        End If
+    End Sub
+
+End Class
